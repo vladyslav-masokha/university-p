6ecdd8ddf24dd5c1453b8cdfae3473457cb96c21
Vladyslav Masokha <vladyslavmasokha2006@gmail.com>
2024-02-15 16:05:22 +0200

    proj pass generate

--- projects/password generator/student_main.py
+++ projects/password generator/student_main.py
@@ -1,26 +1,37 @@
 import random
 import string
+import tkinter as tk
 
-def password_generate(length=12):
+def password_generate():
+    length = int(length_entry.get())
     lowercase_letters = string.ascii_lowercase
     uppercase_letters = string.ascii_uppercase
     digits = string.digits
     special_characters = string.punctuation
-
-    password = random.choice(lowercase_letters) + \
-               random.choice(uppercase_letters) + \
-               random.choice(digits) + \
-               random.choice(special_characters)
-
-    password += ''.join(random.choice(
-      lowercase_letters + uppercase_letters + digits + special_characters
-    ) for _ in range(length))
-
-    password_list = list(password)
-    random.shuffle(password_list)
-    password = ''.join(password_list)
+    
+    password = ''
+    
+    if length < 4:
+        password_label.config(text="Password length must be at least 4 characters")
+    else:
+        for _ in range(length):
+            password += random.choice(lowercase_letters + uppercase_letters + digits + special_characters)
+        password_label.config(text="Generated Password: " + password)
 
     return password
 
-length = int(input("Enter the length of the password: "))
-print("Generated Password:", password_generate(length))
+root = tk.Tk()
+root.title("Password Generator")
+
+length_label = tk.Label(root, text="Password Length:")
+length_label.pack()
+length_entry = tk.Entry(root)
+length_entry.pack()
+
+generate_button = tk.Button(root, text="Generate Password", command=password_generate)
+generate_button.pack()
+
+password_label = tk.Label(root, text="")
+password_label.pack()
+
+root.mainloop()
