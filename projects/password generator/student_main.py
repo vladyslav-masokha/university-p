7bdffaff7932d5899137fac862aa481aa1cf3971
Vladyslav Masokha <vladyslavmasokha2006@gmail.com>
2024-02-16 11:44:24 +0200

    made gui

--- projects/password generator/student_main.py
+++ projects/password generator/student_main.py
@@ -1,37 +1,55 @@
 import random
 import string
-import tkinter as tk
+import PySimpleGUI as sg
 
-def password_generate():
-    length = int(length_entry.get())
-    lowercase_letters = string.ascii_lowercase
-    uppercase_letters = string.ascii_uppercase
-    digits = string.digits
-    special_characters = string.punctuation
+custom_theme = {
+    'BACKGROUND': '#FFFFFF',
+    'TEXT': '#000000',
+    'INPUT': '#FFFFFF',
+    'TEXT_INPUT': '#000000',
+    'SCROLL': '#FFFFFF',
+    'BUTTON': ('#FFFFFF', '#000000'),
+    'PROGRESS': ('#FFFFFF', '#000000'),
+    'BORDER': 1,
+    'SLIDER_DEPTH': 0,
+    'PROGRESS_DEPTH': 0
+}
+
+sg.theme_add_new('CustomTheme', custom_theme)
+sg.theme('CustomTheme')
     
-    password = ''
+layout = [
+    [sg.Text('Uppercase: '), sg.Push(), sg.Input(key='-UP-')],
+    [sg.Text('Lowercase: '), sg.Push(), sg.Input(key='-LOW-')],
+    [sg.Text('Digits: '), sg.Push(), sg.Input(key='-DIT-')],
+    [sg.Text('Characters: '), sg.Push(), sg.Input(key='-CHR-')],
+    [sg.Button('Generate'), sg.Button('Cancel')],
+    [sg.Text('Password: '), sg.Push(), sg.Multiline(no_scrollbar=True, disabled=True, key='-PASS-')]
+]
+
+windows = sg.Window('Password generator', layout)
+
+while True:
+    event, values = windows.read()
     
-    if length < 4:
-        password_label.config(text="Password length must be at least 4 characters")
-    else:
-        for _ in range(length):
-            password += random.choice(lowercase_letters + uppercase_letters + digits + special_characters)
-        password_label.config(text="Generated Password: " + password)
-
-    return password
-
-root = tk.Tk()
-root.title("Password Generator")
-
-length_label = tk.Label(root, text="Password Length:")
-length_label.pack()
-length_entry = tk.Entry(root)
-length_entry.pack()
-
-generate_button = tk.Button(root, text="Generate Password", command=password_generate)
-generate_button.pack()
-
-password_label = tk.Label(root, text="")
-password_label.pack()
-
-root.mainloop()
+    if event == 'Cancel' or event == windows.was_closed:
+        break
+    
+    if event == 'Generate':
+        try:
+            user_upper = int(values['-UP-'])
+            user_lower = int(values['-LOW-'])
+            user_digits = int(values['-DIT-'])
+            user_characters = int(values['-CHR-'])
+            
+            upper = random.sample(string.ascii_uppercase, user_upper)
+            lower = random.sample(string.ascii_lowercase, user_lower)
+            digits = random.sample(string.digits, user_digits)
+            characters = random.sample(string.punctuation, user_characters)
+            
+            password = lower+upper+digits+characters
+            password = random.sample(password, len(password))
+            password = ''.join(password)
+            windows['-PASS-'].update(password)
+        except ValueError:
+            windows['-PASS-'].update('No Valid Number')
