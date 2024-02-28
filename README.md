bf297f17c6a66b84e99c91675175d293e368e186
Vladyslav Masokha <vladyslavmasokha2006@gmail.com>
2024-02-28 22:51:36 +0200

    added lab1, task 1 & 2

---
+++ README.md
@@ -0,0 +1,24 @@
+# Algorithms and Data Structures in C++ Labs
+
+This repository contains a collection of C++ labs focusing on algorithms and data structures. The labs are designed to help you understand and implement various algorithms and data structures commonly used in computer science and software development.
+
+## Getting Started
+
+To get started with the labs, follow these steps:
+
+1. Clone this repository to your local machine.
+   ```
+   git clone https://github.com/your-username/algorithms-and-data-structures-labs.git
+   ```
+
+## Contributing
+
+Contributions to this repository are welcome. If you have any improvements or additional labs to contribute, please fork this repository, make your changes, and submit a pull request. Make sure to follow the existing coding style and documentation conventions.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Acknowledgments
+
+Special thanks to contributors and authors of textbooks and online resources that inspired and informed the creation of these labs.
---
+++ lab1/lab1_2/lab1_2.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+using namespace std;
+
+enum class Brand { APPLE, SAMSUNG, XIAOMI };
+enum class Model { IPHONE, GALAXY, MI };
+
+struct PhoneInfo {
+  Brand brand;
+  Model model;
+  int year;
+  double screenSize;
+};
+
+int main() {
+  PhoneInfo phone;
+  int brandValue, modelValue;
+
+  cout << "Enter the brand of the phone (1 - Apple, 2 - Samsung, 3 - Xiaomi): ";
+  cin >> brandValue;
+  phone.brand = static_cast<Brand>(brandValue);
+
+  cout << "Enter the model of the phone (1 - iPhone, 2 - Galaxy, 3 - MI): ";
+  cin >> modelValue;
+  phone.model = static_cast<Model>(modelValue);
+
+  cout << "Enter the year of release: ";
+  cin >> phone.year;
+
+  cout << "Enter the screen size: ";
+  cin >> phone.screenSize;
+
+  cout << "Phone Brand: ";
+  switch (phone.brand) {
+  case Brand::APPLE:
+    cout << "Apple";
+    break;
+  case Brand::SAMSUNG:
+    cout << "Samsung";
+    break;
+  case Brand::XIAOMI:
+    cout << "Xiaomi";
+    break;
+  }
+  cout << endl;
+
+  cout << "Phone Model: ";
+  switch (phone.model) {
+  case Model::IPHONE:
+    cout << "iPhone";
+    break;
+  case Model::GALAXY:
+    cout << "Galaxy";
+    break;
+  case Model::MI:
+    cout << "MI";
+    break;
+  }
+  cout << endl;
+
+  cout << "Year of release: " << phone.year << endl;
+  cout << "Screen size: " << phone.screenSize << " inches" << endl;
+}
