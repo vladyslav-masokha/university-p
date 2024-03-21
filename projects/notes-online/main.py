c6f78fbc880dbbfa0ef995032b0a88e9863c9223
Vladyslav Masokha <vladyslavmasokha2006@gmail.com>
2024-03-21 15:34:23 +0200

    fix bugs

--- projects/notes-online/main.py
+++ projects/notes-online/main.py
@@ -1,37 +1,26 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 import secrets
-import pickle
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
 
-notes = []
-NOTES_FILE = 'notes.data'
-
 def load_notes():
     try:
-        with open(NOTES_FILE, 'rb') as file:
-            notes = pickle.load(file)
-    except FileNotFoundError:
-        notes = []
-    return notes
+        return session['notes']
+    except KeyError:
+        return []
 
 def save_notes(notes):
-    with open(NOTES_FILE, 'wb') as file:
-        pickle.dump(notes, file)
-
-notes = load_notes()
+    session['notes'] = notes
 
 @app.before_request
 def initialize():
-    load_notes()
-
-@app.teardown_appcontext
-def shutdown(exception=None):
-    save_notes(notes)
+    if 'notes' not in session:
+        session['notes'] = []
 
 @app.route('/')
 def index():
+    notes = load_notes()
     return render_template('index.html', notes=notes)
 
 @app.route('/add', methods=['POST'])
@@ -39,30 +28,36 @@ def add_note():
     note = request.form['note'].strip()
     if note:
         if len(note) <= 200:
+            notes = load_notes()
             notes.append(note)
+            save_notes(notes)
         else:
             flash('Нотатка не може перевищувати 200 символів', 'error')
     else:
         flash('Потрібно ввести текст нотатки', 'error')
     return redirect(url_for('index'))
-  
+
 @app.route('/edit/<int:index>', methods=['GET', 'POST'])
 def edit_note(index):
+    notes = load_notes()
     if request.method == 'POST':
         new_note = request.form['note']
         notes[index] = new_note
+        save_notes(notes)
         return redirect(url_for('index'))
     return render_template('edit.html', note=notes[index], index=index)
 
 @app.route('/delete/<int:index>')
 def delete_note(index):
-    notes.pop(index)
+    notes = load_notes()
+    if 0 <= index < len(notes):
+        notes.pop(index)
+        save_notes(notes)
     return redirect(url_for('index'))
 
 @app.route('/clear')
 def clear_notes():
-    global notes
-    notes = []
+    session.pop('notes', None)
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
