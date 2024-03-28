f695a273eec3fe2ef52c419ef033338665ed2485
Vladyslav Masokha <vladyslavmasokha2006@gmail.com>
2024-03-28 19:25:00 +0200

    fix bugs lab8

--- labs/lab8/student_main.py
+++ labs/lab8/student_main.py
@@ -1,4 +1,5 @@
 import json
+from jsonschema import validate, ValidationError
 
 def task1(path, age):
     with open(path, 'r') as file:
@@ -6,6 +7,52 @@ def task1(path, age):
 
     names = [person['name'] for person in data if person['age'] > age]
     return names
-  
-task1('data.json', 30)
 
+def task2(data, path):
+  dataJson = json.dumps(data)
+  
+  with open(path, 'w') as file:
+    file.write(dataJson)
+    
+def task3(schema, paths):
+  invalidFiles = []
+  
+  for path in paths:
+    try:
+      with open(path, 'r') as file:
+        data = json.load(file)
+        validate(instance=data, schema=schema)
+    except ValidationError:
+      invalidFiles.append(path)
+  
+  return invalidFiles
+
+def task4(path, key):
+    with open(path, 'r') as file:
+        data = json.load(file)
+    values = []
+
+    def convert_data(data):
+        if isinstance(data, dict):
+            for k, v in data.items():
+                if k == key:
+                    values.append(v)
+                else:
+                    convert_data(v)
+        elif isinstance(data, list):
+            for item in data:
+                convert_data(item)
+
+    convert_data(data)
+    return values
+
+def task5(path, category, func):
+    with open(path, 'r') as file:
+        data = json.load(file)
+        
+    for item in data:
+        if item['category'] == category:
+            func(item)
+            
+    with open(path, 'w') as file:
+        json.dump(data, file)
