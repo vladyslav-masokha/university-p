6c513e0fddc38c01634dd17efa111d71bbd157b0
Vladyslav Masokha <vladyslavmasokha2006@gmail.com>
2024-05-02 20:51:11 +0300

    added lab2

---
+++ lab2/lab2_2/lab2_2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string str, modificatedStr;
+
+  cout << "Enter your string: ";
+  getline(cin, str);
+
+  for (char l : str) {
+    if (l != ' ') modificatedStr += l;
+  }
+
+  cout << modificatedStr << endl;
+}
